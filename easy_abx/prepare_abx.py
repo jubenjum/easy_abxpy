959aa9213c577dc1b1cd2b142ae8f88508f1f274
Juan Benjumea <jubenjum@gmail.com>
2018-01-17 16:28:17 +0100

    added projectil file

--- easy_abx/prepare_abx.py
+++ easy_abx/prepare_abx.py
@@ -24,15 +24,15 @@ def main():
     parser.add_argument('csv_file', help='input csv file')
 
     parser.add_argument('output_name', help='experiment name, output file name of abx files')
-    
+
     parser.add_argument('--col_items', help='columns with the file name or items, columns begins in 1')
 
     parser.add_argument('--col_coords', help='columns with the coords, columns begins in 1')
 
-    parser.add_argument('--col_labels', required=True, 
+    parser.add_argument('--col_labels', required=True,
             help='columns with the labels, columns begins in 1')
- 
-    parser.add_argument('--col_features', nargs='*', required=True, 
+
+    parser.add_argument('--col_features', nargs='*', required=True,
             help='columns with the features name, columns begins in 1')
 
     parser.add_argument('--header', action='store_true', help='first line in the csv file is a header')
@@ -45,7 +45,7 @@ def main():
 
     # options to pass to  create_abx_files function
     cmd_options = ['col_items', 'col_coords', 'col_labels', 'col_features', 'no_header']
-    options = namedtuple('options', cmd_options) 
+    options = namedtuple('options', cmd_options)
 
     # it's easier to count columns from one, I transform that to pythons indexes (0 based)
     ranges = lambda x: list(np.array(parse_ranges(x))-1)
@@ -54,25 +54,25 @@ def main():
     options.col_labels = ranges(args.col_labels) if args.col_labels else None
     options.col_features = ranges(args.col_features[0]) if args.col_features else None
     options.header = args.header
-   
+
     is_matrix = True
     if options.header:
         try:
             df = pd.read_csv(input_csv)
         except pd.errors.ParserError:
             is_matrix = False
-            
+
     else:
         try:
             df = pd.read_csv(input_csv, header=None)
         except pd.errors.ParserError:
             is_matrix = False
-    
+
     if not is_matrix:
         print('ABX score is computed from fixed length features' +
-                ': {} has mixed lengths'.format(input_csv))  
+                ': {} has mixed lengths'.format(input_csv))
         sys.exit()
-    
+
     create_abx_files(df, options, output_name)
 
 
